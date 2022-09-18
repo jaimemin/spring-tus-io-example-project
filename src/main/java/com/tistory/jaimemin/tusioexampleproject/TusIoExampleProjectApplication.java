b753727cde49b7753c4e84d1ca442d0144c4b515
jaimemin <jaimemin@gmail.com>
2022-09-19 00:10:52 +0900

    first commit

---
+++ src/main/java/com/tistory/jaimemin/tusioexampleproject/TusIoExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.tusioexampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusIoExampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusIoExampleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tistory/jaimemin/tusioexampleproject/TusIoExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.tusioexampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusIoExampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
