a51f902af5df229928e1a54b2c297c348ad86d8d
jaimemin <jaimemin@gmail.com>
2022-10-03 22:58:01 +0900

    비교를 위해 일반 파일 업로드 구현

--- src/main/java/com/tistory/jaimemin/tusioexampleproject/controller/MainController.java
+++ src/main/java/com/tistory/jaimemin/tusioexampleproject/controller/MainController.java
@@ -1,13 +1,44 @@
 package com.tistory.jaimemin.tusioexampleproject.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/upload")
+    public String test() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile(@RequestParam MultipartFile file
+            , HttpServletRequest request) throws IOException {
+        log.info("request = {}", request);
+        log.info("multipartFile = {}", file);
+
+        if (!file.isEmpty()) {
+            File dir = new File("dest/" + file.getOriginalFilename());
+
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+        }
+
+        return "upload-form";
     }
 }
